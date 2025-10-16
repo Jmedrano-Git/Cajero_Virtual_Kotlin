6d644bfd50ea06da2376e85b7bed25a9b9b180bc
Julio Medrano <julio.medrano@tecsup.edu.pe>
2025-10-16 12:45:37 -0500

    Starting the project

---
+++ app/src/main/java/com/medrano/tarea_s8/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.medrano.tarea_s8
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.medrano.tarea_s8.ui.theme.Tarea_S8Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Tarea_S8Theme {
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+}
