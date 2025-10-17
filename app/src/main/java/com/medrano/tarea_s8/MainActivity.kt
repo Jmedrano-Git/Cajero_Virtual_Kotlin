0affdf9c540061421fd7bda1459ddc1a34e2dbe2
Julio Medrano <julio.medrano@tecsup.edu.pe>
2025-10-17 03:08:22 -0500

    Imports

--- app/src/main/java/com/medrano/tarea_s8/MainActivity.kt
+++ app/src/main/java/com/medrano/tarea_s8/MainActivity.kt
@@ -4,7 +4,8 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
