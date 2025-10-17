bd1ce4a8f126fcd60465a8a440e01307e868ba9a
Julio Medrano <julio.medrano@tecsup.edu.pe>
2025-10-17 00:02:19 -0500

    F

--- app/src/main/java/com/medrano/tarea_s8/MainActivity.kt
+++ app/src/main/java/com/medrano/tarea_s8/MainActivity.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
