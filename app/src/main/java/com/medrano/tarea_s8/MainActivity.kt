cc2cb7b9547e812d781ecb8f793222b896858dc1
Julio Medrano <julio.medrano@tecsup.edu.pe>
2025-10-17 00:02:24 -0500

    f

--- app/src/main/java/com/medrano/tarea_s8/MainActivity.kt
+++ app/src/main/java/com/medrano/tarea_s8/MainActivity.kt
@@ -12,6 +12,8 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.medrano.tarea_s8.ui.theme.Tarea_S8Theme
