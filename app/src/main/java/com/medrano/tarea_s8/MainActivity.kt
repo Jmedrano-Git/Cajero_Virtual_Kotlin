1115243a283fe15daebad86d5fbe30df63e7de98
Julio Medrano <julio.medrano@tecsup.edu.pe>
2025-10-17 03:08:32 -0500

    Imports

--- app/src/main/java/com/medrano/tarea_s8/MainActivity.kt
+++ app/src/main/java/com/medrano/tarea_s8/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
