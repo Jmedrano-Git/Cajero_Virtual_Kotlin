51e80da62f7c5481b896d85988935ffad15b2805
Julio Medrano <julio.medrano@tecsup.edu.pe>
2025-10-17 03:11:57 -0500

    Creacion de Tema

--- app/src/main/java/com/medrano/tarea_s8/ui/theme/Theme.kt
+++ app/src/main/java/com/medrano/tarea_s8/ui/theme/Theme.kt
@@ -10,14 +10,8 @@ private val PurpleDark = Color(0xFF3A2BA2)
 private val AccentGreen = Color(0xFF00C853)
 private val AccentRed = Color(0xFFD50000)
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
+private val AppColorScheme = lightColorScheme(
+    primary = PurplePrimary,
     onPrimary = Color.White,
     onSecondary = Color.White,
     onTertiary = Color.White,
